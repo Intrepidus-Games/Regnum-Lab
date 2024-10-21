59135125b142135b8adc887f4799de41670ce28f
Monnapse <masonshuber@gmail.com>
2024-10-20 20:51:48 -0600

    Update client.js

--- client/client.js
+++ client/client.js
@@ -4,11 +4,12 @@ REGNUMLAB.addEvent("update", update)
 const box2 = REGNUMLAB.scene.createBox("box", new THREE.Vector3(0.3, 0.3, 0.3), 0x00FF00);
 box2.position.x = 1;
 
-
-const dotProduct = box1.position.dot(box2.position);
+const dotProduct = new THREE.Vector3().dot(box2.position);
 console.log(dotProduct);
 
 function update(deltaTime)
 {
-    rotateAround(box2, new THREE.Vector3, new THREE.Vector3(0,1,1), true);
+    
+    rotateAround(box2, new THREE.Vector3, new THREE.Vector3(1,1,1), true);
+    
 }
